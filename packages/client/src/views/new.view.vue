e3bb7ecef62549c391fd8b370bd1cbe09c554250
Christian Ulbrich <christian.ulbrich@zalari.de>
2023-07-12 14:58:36 +0200

    feat: mv new view

---
+++ packages/client/src/views/new.view.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import NewMeeting from '../components/new-meeting.component.vue';
+import JoinMeeting from '../components/join-meeting.component.vue';
+
+export default {
+  components: { NewMeeting, JoinMeeting },
+};
+</script>
+<template>
+  <!--display=block hack to make always visible on mobile -->
+  <nav class="navbar" role="navigation" aria-label="main navigation" style="display: block">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/" id="tcq-logo">TCQ</a>
+      <span style="flex-grow: 1"></span>
+      <a href="/logout" class="navbar-item">Log Out</a>
+    </div>
+  </nav>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-6">
+        <div class="box">
+          <div class="title is-4">Join Meeting</div>
+          <div id="join-meeting">
+            <JoinMeeting></JoinMeeting>
+          </div>
+        </div>
+      </div>
+      <div class="column is-6">
+        <div class="box">
+          <div class="title is-4">New Meeting</div>
+          <div id="new-meeting">
+            <NewMeeting></NewMeeting>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<style lang="scss">
+$navbar-background-color: #fed;
+$navbar-item-hover-background-color: lighten($navbar-background-color, 10%);
+@import 'bulma/bulma';
+@import '../styles/navbar.scss';
+
+.box {
+  min-height: 200px;
+}
+</style>
--- src/client/pages/meeting/meeting.ts
+++
@@ -1,4 +0,0 @@
-import App from '../../components/App/app';
-import './meeting.scss';
-// apply template
-new App({ el: '#app' });
