496d68551a901ad8d7fb437c1bde5a3ec7821bb9
Tom Kopp <tom.kopp@zalari.de>
2023-11-14 10:42:22 +0100

    feat(server): add log lvl env and upadte env related error messages

--- packages/server/src/secrets.ts
+++ packages/server/src/secrets.ts
@@ -6,21 +6,21 @@ export const SESSION_SECRET = process.env.SESSION_KEY as string; // Program will
 export const CDB_SECRET = process.env.DB_KEY;
 
 if (!GITHUB_CLIENT_SECRET) {
-  log.fatal('ERROR\tNo client secret. Set TCQ_GH_SECRET.');
+  log.fatal('ERROR\tNo client secret. Set OAUTH_CLIENT_SECRET.');
   process.exit(1);
 }
 
 if (!GITHUB_CLIENT_ID) {
-  log.fatal('ERROR\tNo client id. Set TCQ_GH_ID.');
+  log.fatal('ERROR\tNo client id. Set OAUTH_CLIENT_ID.');
   process.exit(1);
 }
 
 if (!SESSION_SECRET) {
-  log.fatal('ERROR\tNo session secret. Set TCQ_SESSION_SECRET.');
+  log.fatal('ERROR\tNo session secret. Set SESSION_KEY.');
   process.exit(1);
 }
 
 if (!CDB_SECRET) {
-  log.fatal('ERROR\tNo CosmosDB secret. Set TCQ_CDB_SECRET.');
+  log.fatal('ERROR\tNo CosmosDB secret. Set DB_KEY.');
   process.exit(1);
 }
