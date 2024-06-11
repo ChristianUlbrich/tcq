01183b9303dc7ee8dce3741d5340f17a49568a3e
Tom Kopp <tom.kopp@zalari.de>
2024-06-11 11:56:17 +0300

    feat(server): exchange DB engine to lowdb

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -1,77 +1,27 @@
-export const HOST = process.env.DB_URI;
-export const DATABASE_ID = process.env.DB_NAME;
-export const COLLECTION_ID = process.env.DB_CONTAINER;
-export const PARTITION_KEY = process.env.DB_PARTITION_KEY;
-export const PARTITION_VALUE = process.env.DB_PARTITION_VALUE;
-export const SESSION_COLLECTION_ID = process.env.DB_CONTAINER_SESSIONS;
+import { JSONFilePreset } from 'lowdb/node';
+import { resolve } from 'node:path';
+import type Meeting from '../../shared/dist/Meeting.d.ts';
 
-import log from './logger.js';
-import { CDB_SECRET } from './secrets.js';
-import { CosmosClient, Resource } from '@azure/cosmos';
-import Meeting from '../../shared/dist/Meeting.js';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-if (!HOST) {
-  log.fatal('ERROR\tNo database endpoint. Set DB_URI.');
-  process.exit(1);
-}
-if (!DATABASE_ID) {
-  log.fatal('ERROR\tNo database name. Set DB_NAME.');
-  process.exit(1);
-}
-if (!COLLECTION_ID) {
-  log.fatal('ERROR\tNo database container / collection. Set DB_CONTAINER.');
-  process.exit(1);
-}
+type Data = { meetings: Meeting[]; };
 
-const dbClient = new CosmosClient({
-  endpoint: HOST,
-  key: CDB_SECRET
-});
-
-await dbClient
-  .databases
-  .createIfNotExists({ id: DATABASE_ID, maxThroughput: 1000 });
-await dbClient
-  .database(DATABASE_ID)
-  .containers
-  .createIfNotExists({ id: COLLECTION_ID, partitionKey: PARTITION_KEY });
-
-async function upsertMeeting(meeting: Meeting) {
-  const { resource } = await dbClient
-    .database(DATABASE_ID!)
-    .container(COLLECTION_ID!)
-    .items
-    .upsert<Meeting>(meeting);
-  return resource;
-}
-
-async function updateMeeting(meeting: Meeting) {
-  await upsertMeeting(meeting);
-}
-
-async function getMeeting(meetingId: string): Promise<(Meeting & Resource) | undefined> {
-  const { resource } = await dbClient
-    .database(DATABASE_ID!)
-    .container(COLLECTION_ID!)
-    .item(meetingId, PARTITION_VALUE)
-    .read<Meeting>()
-    .then(
-      doc => {
-        if (doc.statusCode > 299) {
-          return Promise.reject({ message: 'Meeting not found.', status: doc.statusCode });
-        }
-        return doc;
-      }
-    );
-  return resource;
-}
-
-async function createMeeting(meeting: Meeting): Promise<(Meeting & Resource) | undefined> {
-  return upsertMeeting(meeting);
-}
-
-export {
-  createMeeting,
-  getMeeting,
-  updateMeeting
+const defaultData = {
+  meetings: []
 };
+const db = await JSONFilePreset<Data>(resolve(__dirname, './', 'db.json'), defaultData);
+
+const upsertMeeting = async (meeting: Meeting) => {
+  const dat = db.data.meetings.filter((element: any) => element.id === meeting.id).at(0);
+
+  if (dat) Object.assign(dat, meeting);
+  else db.data.meetings.push(meeting);
+
+  return db.write();
+};
+
+export const getMeeting = async (meetingId: string) => db.data.meetings.find((m: any) => m.id === meetingId);
+export const createMeeting = upsertMeeting;
+export const updateMeeting = upsertMeeting;
